b4ff873961b4a3bad4e16ce8ef855dda7abbd5b8
Arpi Vezer <arpi.vezer@gmail.com>
2019-07-16 12:56:21 +0100

    pass numpy headers path to setup to fix build on Mac OS

--- lib/setup.py
+++ lib/setup.py
@@ -1,17 +1,21 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+import numpy as np
 
 setup(
-  name = 'ebcubed',
-  ext_modules = cythonize("ebcubed.pyx"),
+    name='ebcubed',
+    ext_modules=cythonize("ebcubed.pyx"),
+    include_dirs=[np.get_include()],
 )
 
 setup(
-  name = 'jaccard',
-  ext_modules = cythonize("jaccard.pyx"),
+    name='jaccard',
+    ext_modules=cythonize("jaccard.pyx"),
+    include_dirs=[np.get_include()],
 )
 
 setup(
-  name = 'cfisher',
-  ext_modules = cythonize("cfisher.pyx"),
+    name='cfisher',
+    ext_modules=cythonize("cfisher.pyx"),
+    include_dirs=[np.get_include()],
 )
